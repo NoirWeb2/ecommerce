855546217fe6cfefc84e41d25e93968798777862
noirLovers <hello@noirlovers.com>
2026-05-31 16:17:48 -0500

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,7 @@
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+return <>{children}</>;
 }
